8825a6dd4a14c26621d33671a27d6809bc6b942c
perezjuang <perezjuang@hotmail.com>
2022-02-12 00:51:59 -0500

    UPDATE

---
+++ fxJava/JavaRestClient/src/main/java/module-info.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+/**
+ * @author perez
+ *
+ */
+module MouseMover {
+	exports fxts.stations.transport.tradingapi.requests;
+	exports com.fxcm.external.api.transport.exception;
+	exports fxts.stations.trader.ui.dialogs;
+	exports fxts.stations.util.signals;
+	exports test.strategy.stochastic;
+	exports fxts.stations.trader.ui.actions;
+	exports pulling.historical.prices.stochastic;
+	exports fxts.stations.datatypes;
+	exports fxts.stations.ui;
+	exports fxts.stations.transport.tradingapi.processors;
+	exports fxts.stations.transport.tradingapi;
+	exports com.fxcm.external.api.transport;
+	exports fxts.stations.trader.ui.frames;
+	exports fxts.stations.util;
+	exports com.fxcm.internal.transport;
+	exports fxts.stations.transport;
+	exports fxts.stations.trader;
+	exports com.fxcm.external.api.transport.listeners;
+	exports fxts.stations.core;
+	exports fxts.stations.transport.tradingapi.resources;
+	exports com.fxcm.external.api.transport.sso;
+	exports fxts.stations.ui.colorchooser;
+	exports fxts.stations.ui.help;
+	exports com.fxcm.external.api.util;
+	exports fxts.stations.trader.ui;
+	exports fxts.stations.util.preferences;
+
+	requires fxcm.api;
+	requires fxmsg;
+	requires java.desktop;
+	requires java.xml;
+}
